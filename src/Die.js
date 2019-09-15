dc8b7e107219ed41b6f0eb23c14692e3ec56584c
Victor Marks <vjmarks@gmail.com>
2019-09-14 22:12:17 -0700

    locked correct dice and added logic for full house and small straight

--- src/Die.js
+++ src/Die.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import "./Die.css";
+import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
   render() {
     return (
       <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        className={'Die'}
+        style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
+        onClick={() => this.props.handleClick(this.props.idx)}
       >
         {this.props.val}
       </button>
